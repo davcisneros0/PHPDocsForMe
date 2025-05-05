1badda2f98f4b3367d98b77324ff2e3e4402cd99
David <Davcisneros2000@gmail.com>
2025-05-04 21:05:20 -0400

    Add SQLite database and try/catch to connect to db

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -2,3 +2,24 @@
 
 require 'functions.php';
 require 'router.php';
+
+// connect to SQLite database
+
+$db = 'demo.db';
+
+try {
+    $pdo = new PDO("sqlite:{$db}"); 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $statement = $pdo->prepare("SELECT * FROM posts");
+    $statement->execute();
+
+    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    echo "Error via database: " . $e->getMessage();
+}
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
