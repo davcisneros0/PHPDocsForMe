1d314969bef6081c9cfa4efc02ab73e18fd67bbb
David <Davcisneros2000@gmail.com>
2025-05-11 09:55:27 -0400

    Add PHP input validation/ if statements to prevent invalid form

--- websites/demo/controllers/note-create.php
+++ websites/demo/controllers/note-create.php
@@ -5,10 +5,23 @@ $db = new Database();
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+    $errors = [];
+
+    // confirm if body has text
+    if (strlen($_POST['body']) == 0) {
+        $errors['body'] = "A body is required...";
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = "The body can not be more than 1,000 characters...";
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
  
 require 'views/note-create.view.php';
