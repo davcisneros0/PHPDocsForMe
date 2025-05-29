b6c296cc8faf1822df43d83ff235cd94dba99cb7
David <Davcisneros2000@gmail.com>
2025-05-28 22:06:35 -0400

    Add PHP for delete button for each note

--- websites/demo/controllers/note.php
+++ websites/demo/controllers/note.php
@@ -2,21 +2,47 @@
 
 $dbb = new Database();
 
+$currentUserId = 23;
 $heading = "Note";
-$currentUserId = 1;
-
-$note = $dbb->query('SELECT * from notes WHERE id = :id', [
-    'id' => $_GET['id']
-    ])->fetch();
 
 
-if (!$note) {
-    abort();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $note = $dbb->query('SELECT * from notes WHERE id = :id', [
+        'id' => $_GET['id']
+        ])->fetch();
+
+
+    if (!$note) {
+        abort();
+    }
+
+    if ($note['user_id'] != $currentUserId) {
+        abort(Response::FORBIDDEN);
+    }
+
+    $dbb->query('DELETE FROM notes WHERE id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+
+} else {
+
+    $currentUserId = 1;
+
+    $note = $dbb->query('SELECT * from notes WHERE id = :id', [
+        'id' => $_GET['id']
+        ])->fetch();
+
+
+    if (!$note) {
+        abort();
+    }
+
+    if ($note['user_id'] != $currentUserId) {
+        abort(Response::FORBIDDEN);
+    }
+
 }
-
-if ($note['user_id'] != $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
-
-
 require "views/note.view.php"; 
--- websites/demo/views/note.view.php
+++ websites/demo/views/note.view.php
@@ -12,6 +12,11 @@
     </p>
 
         
+   <form class="mt-6" method="POST">
+      <input type="hidden" name="id" value="<?= $note['id'] ?>">
+      <button class="text-sm text-red-500">Delete</button>
+   </form>
+
     </div>
   </main>
 </div>
